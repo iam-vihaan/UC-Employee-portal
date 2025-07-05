a4da656849046b27eb845cfc164ed6c88493bf16
iam-vihaan <asathyavihaan@gmail.com>
2025-07-05 14:49:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 # backend/app.py
 from flask import Flask
 from routes import employee_routes
+from auth import init_jwt
+from models import db
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+init_jwt(app)
+db.init_app(app)
 app.register_blueprint(employee_routes)
 
 @app.route("/health")
